6699ebd667dcf1839d74df7707f0300a57ba207b
33Joo33 <147119929+33Joo33@users.noreply.github.com>
2025-11-13 19:06:01 +0900

    Update patch_rustdesk.ps1

--- .github/workflows/patch_rustdesk.ps1
+++ .github/workflows/patch_rustdesk.ps1
@@ -1,25 +1,26 @@
-# ==========================
-# RustDesk 자동 서버 패치 스크립트
-# ==========================
+# Patch RustDesk config (server, api, key)
 
 $server = "koo2355.synology.me"
 $key = "2Nplw2eqfhdJxP1P+f0TD3SJMRCpLGA4FAUbnawO5e8="
 
 $configFile = ".\src\common\config.rs"
 
+# Replace Rendezvous Server
 (Get-Content $configFile) |
 ForEach-Object {
     $_ -replace 'DEFAULT_RENDEZVOUS_SERVER: &str = ".*?"', "DEFAULT_RENDEZVOUS_SERVER: &str = `"$server:21116`""
 } | Set-Content $configFile
 
+# Replace API Server
 (Get-Content $configFile) |
 ForEach-Object {
     $_ -replace 'DEFAULT_API_SERVER: &str = ".*?"', "DEFAULT_API_SERVER: &str = `"$server`""
 } | Set-Content $configFile
 
+# Replace Public Key
 (Get-Content $configFile) |
 ForEach-Object {
     $_ -replace 'DEFAULT_PUBLIC_KEY: &str = ".*?"', "DEFAULT_PUBLIC_KEY: &str = `"$key`""
 } | Set-Content $configFile
 
-Write-Host "패치 완료!"
+Write-Output "PATCH_OK"
